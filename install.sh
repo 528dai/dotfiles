c61ec24d58a85c012f30dc5f0bd1fdbe09107ac2
Daiki Ito <528dai@gmail.com>
2017-11-23 22:16:25 +0900

    Remove install zsh neovim command

--- install.sh
+++ install.sh
@@ -13,23 +13,12 @@ fi
 
 set -eo pipefail
 
-case "$OS" in
-  'Mac') brew install zsh;;
-  'Linux') sudo apt-get install zsh;; 
-  *) echo "$OS Didn't match anything"
-esac
-
-ln -sf ~/dotfiles//zsh/zshrc ~/.zshrc
+ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
 
 curl -fLo \
      ~/dotfiles/vim/plug/autoload/plug.vim --create-dirs \
      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-case "$OS" in
-  'Mac') brew install neovim;;
-  'Linux') sudo apt-get install neovim;; 
-  *) echo "$OS Didn't match anything"
-esac
 pip3 install --user --upgrade neovim
 ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
 ln -sf ~/dotfiles/vim ~/.vim
