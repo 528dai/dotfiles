9d9d05dd78cfa91ff0d0c2902476d64f0f9fd2b5
Daiki Ito <528dai@gmail.com>
2017-11-23 23:40:48 +0900

    Add install_min.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ if [ "$(uname)" == 'Darwin' ]; then
   OS='Mac'
 elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
   OS='Linux'
-elif [ "$(expr substr $(uname -s) 1 10)" == 'MINGW32_NT' ]; then                                                                                           
+elif [ "$(expr substr $(uname -s) 1 10)" == 'MINGW32_NT' ]; then
   OS='Cygwin'
 else
   echo "Your platform ($(uname -a)) is not supported."
@@ -22,6 +22,7 @@ curl -fLo \
 pip3 install --user --upgrade neovim
 ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
 ln -sf ~/dotfiles/vim ~/.vim
+mkdir -p ~/.config/nvim
 ln -sf ~/dotfiles/vim/vimrc ~/.config/nvim/init.vim
 
 nvim --headless +PlugUpgrade +PlugClean! +PlugUpdate +qall
