574c00921d320cb6fd0e6efa8ecd70fc2e10c32d
Daiki Ito <528dai@gmail.com>
2017-12-19 22:10:45 +0900

    Modify install.sh

--- install.sh
+++ install.sh
@@ -19,13 +19,8 @@ curl -fLo \
      ~/dotfiles/vim/plug/autoload/plug.vim --create-dirs \
      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-pip3 install --user --upgrade neovim
 ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
 ln -sf ~/dotfiles/vim ~/.vim
-mkdir -p ~/.config/nvim
-ln -sf ~/dotfiles/vim/vimrc ~/.config/nvim/init.vim
-
-nvim --headless +PlugUpgrade +PlugClean! +PlugUpdate +qall
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ln -sf ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
