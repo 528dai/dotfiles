586846b8a5c4d2d19c26cd7542b87ce65265ebd6
Daiki Ito <528dai@gmail.com>
2020-08-05 21:58:47 +0900

    iroiro

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ "$(uname)" == 'Darwin' ]; then
-  OS='Mac'
+  os='Mac'
 elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
-  OS='Linux'
+  os='Linux'
 elif [ "$(expr substr $(uname -s) 1 10)" == 'MINGW32_NT' ]; then
-  OS='Cygwin'
+  os='Cygwin'
 else
   echo "Your platform ($(uname -a)) is not supported."
   exit 1
@@ -14,6 +14,7 @@ fi
 set -eo pipefail
 
 ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
+# zlug install
 
 ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
 ln -sf ~/dotfiles/vim ~/.vim
@@ -21,6 +22,6 @@ ln -sf ~/dotfiles/vim ~/.vim
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ln -sf ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
 
-ln -sf ~/dotfiles/alacritty/alacritty.yml ~/.alacritty.yml
+# ln -sf ~/dotfiles/alacritty/alacritty.yml ~/.alacritty.yml
 
-ln -sf ~/dotfiles/gdb/gdb-dashboard/.gdbinit ~/.gdbinit
+# ln -sf ~/dotfiles/gdb/gdb-dashboard/.gdbinit ~/.gdbinit
